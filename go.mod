258546d22ce3e17804bbdac7082dedb6eb35cc81
Loren Froomin <lfroomin@hey.com>
2023-04-14 08:26:18 -0700

    Add CORS handling and middleware to log requests

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression v1.4.48
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.19.4
 	github.com/aws/aws-sdk-go-v2/service/location v1.22.5
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.0
 	github.com/google/go-cmp v0.5.9
 	github.com/google/uuid v1.3.0
--- server/router.go
+++ server/router.go
@@ -1,14 +1,24 @@
 package server
 
 import (
+	"bytes"
+	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/lfroomin/restaurant-container/controllers"
+	"io"
+	"log"
+	"strings"
 )
 
 func NewRouter(env Env) *gin.Engine {
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.Use(gin.Recovery())
+	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowCredentials = true
+	router.Use(cors.New(config))
+
+	router.Use(logRequest)
 
 	restaurant := controllers.RestaurantController{
 		Restaurant: env.Restaurant,
@@ -23,4 +33,19 @@ func NewRouter(env Env) *gin.Engine {
 	idGrp.DELETE("", restaurant.Delete)
 
 	return router
+}
+
+func logRequest(c *gin.Context) {
+	byteBody, _ := io.ReadAll(c.Request.Body)
+	c.Request.Body = io.NopCloser(bytes.NewBuffer(byteBody))
+
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("\n  Request: [%s] %s%s (%s)\n", c.Request.Method, c.Request.Host, c.Request.URL, c.Request.Proto))
+	sb.WriteString(fmt.Sprintf("  Header: %+v\n", c.Request.Header))
+	if len(byteBody) > 0 {
+		sb.WriteString(fmt.Sprintf("  Body: %s\n", string(byteBody)))
+	}
+	log.Print(sb.String())
+
+	c.Next()
 }
